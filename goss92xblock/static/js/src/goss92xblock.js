fc3d69f8883d68b882657681ee52aa61309f420e
gossoudarev <goss@itmo.ru>
2020-09-28 02:22:14 +0300

    first commit

---
+++ goss92xblock/static/js/src/goss92xblock.js
@@ -0,0 +1,17 @@
+/* Javascript for goss8XBlock. */
+function Goss92XBlock(runtime, element) {
+
+    window.updateScore = function (result) {
+        $('.score', element).text(result.score);
+    }
+
+    window.handlerUrl = runtime.handlerUrl(element, 'set_score2');
+
+    $('#gosssend').click(buttonClick);
+
+
+    $(function ($) {
+        console.log('*x92***********');
+        /* Here's where you'd do things on page load. */
+    });
+}
